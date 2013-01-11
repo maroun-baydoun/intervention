ee864c5f807f0802f8a30b90be5fad0873d01cca
marounbaydoun <maroun.baydoun@gmail.com>
2013-01-11 10:51:39 +0200

    Exception refactoring in DynamicScriptEngine

--- src/main/java/com/mb/intervention/script/DynamicScriptEngine.java
+++ src/main/java/com/mb/intervention/script/DynamicScriptEngine.java
@@ -1,22 +1,24 @@
-/**Copyright 2012 Maroun Baydoun
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-**/
-
+/**
+ * Copyright 2012 Maroun Baydoun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+*
+ */
 package com.mb.intervention.script;
 
 import com.mb.intervention.ObjectFactory;
 import com.mb.intervention.context.Context;
+import com.mb.intervention.exceptions.InterventionException;
 import com.mb.intervention.log.LocalizedLogger;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -29,21 +31,17 @@ import javax.script.*;
 
 public class DynamicScriptEngine {
 
-    private static final String DYNAMIC_LANGUAGE=ObjectFactory.getInstance().getContext().getConfiguration().getDynamicLanguage();
-    private static final String SCRIPT_EXTENSION=ObjectFactory.getInstance().getContext().getConfiguration().getScriptExtension();
-    private static final String DEFAULT_SCRIPT=ObjectFactory.getInstance().getContext().getConfiguration().getDefaultScript();
-    private static final String SCRIPT_LOCATION=ObjectFactory.getInstance().getContext().getConfiguration().getScriptLocation();
-    private static final String SCRIPT_LOCATION_TYPE=ObjectFactory.getInstance().getContext().getConfiguration().getScriptLocationType();
-    
-    
+    private static final String DYNAMIC_LANGUAGE = ObjectFactory.getInstance().getContext().getConfiguration().getDynamicLanguage();
+    private static final String SCRIPT_EXTENSION = ObjectFactory.getInstance().getContext().getConfiguration().getScriptExtension();
+    private static final String DEFAULT_SCRIPT = ObjectFactory.getInstance().getContext().getConfiguration().getDefaultScript();
+    private static final String SCRIPT_LOCATION = ObjectFactory.getInstance().getContext().getConfiguration().getScriptLocation();
+    private static final String SCRIPT_LOCATION_TYPE = ObjectFactory.getInstance().getContext().getConfiguration().getScriptLocationType();
     private static DynamicScriptEngine instance;
     private static DynamicScriptEngine defaultInstance;
     private ScriptEngine engine;
     private Invocable invocableEngine;
     private Compilable compilableEngine;
     private Map<String, CompiledScript> compiledScripts;
-    
-    
 
     private DynamicScriptEngine(String language) {
 
@@ -71,70 +69,67 @@ public class DynamicScriptEngine {
         return defaultInstance;
     }
 
-    private CompiledScript getCompiledScript(String scriptName) throws ScriptException {
+    private CompiledScript getCompiledScript(String scriptName) throws ScriptException, InterventionException {
 
         CompiledScript script = compiledScripts.get(scriptName);
 
         if (script == null) {
-            InputStream scriptStream =getScriptAsStream(scriptName);
+            InputStream scriptStream = getScriptAsStream(scriptName);
+
+            Reader scriptReader = new InputStreamReader(scriptStream);
+
+            script = compilableEngine.compile(scriptReader);
             
-            if (scriptStream != null) {
-                Reader scriptReader = new InputStreamReader(scriptStream);
-
-                script = compilableEngine.compile(scriptReader);
-                compiledScripts.put(scriptName, script);
-
-            } else {
-                if (!scriptName.equals(DEFAULT_SCRIPT)) {
-                    LocalizedLogger.severe(DynamicScriptEngine.class.getName(), "script_file_not_found", scriptName,SCRIPT_EXTENSION,SCRIPT_LOCATION);
-                }
-
-            }
+            compiledScripts.put(scriptName, script);
         }
 
         return script;
     }
 
-    private static InputStream getScriptAsStream(String scriptName){
-     
-        InputStream stream=null;
-        
-       
-        if(SCRIPT_LOCATION_TYPE.equals(Context.Configuration.SCRIPT_LOCATION_CLASSPATH)){
-            
-            stream=DynamicScriptEngine.class.getResourceAsStream(SCRIPT_LOCATION+""+scriptName+"."+SCRIPT_EXTENSION);
-        }
-        else if(SCRIPT_LOCATION_TYPE.equals(Context.Configuration.SCRIPT_LOCATION_FOLDER)){
-            
-            try{
-                stream=new FileInputStream(SCRIPT_LOCATION+""+scriptName+"."+SCRIPT_EXTENSION);
+    private static InputStream getScriptAsStream(String scriptName) throws InterventionException {
+
+        InputStream stream = null;
+
+
+        if (SCRIPT_LOCATION_TYPE.equals(Context.Configuration.SCRIPT_LOCATION_CLASSPATH)) {
+
+            stream = DynamicScriptEngine.class.getResourceAsStream(SCRIPT_LOCATION + "" + scriptName + "." + SCRIPT_EXTENSION);
+
+            if (stream == null) {
+                throw new InterventionException("script_file_not_found", scriptName, SCRIPT_EXTENSION);
             }
-            catch(FileNotFoundException ex){
-                
+        } else if (SCRIPT_LOCATION_TYPE.equals(Context.Configuration.SCRIPT_LOCATION_FOLDER)) {
+
+            try {
+                stream = new FileInputStream(SCRIPT_LOCATION + "" + scriptName + "." + SCRIPT_EXTENSION);
+            } catch (FileNotFoundException ex) {
+                throw new InterventionException("script_file_not_found", ex, scriptName, SCRIPT_EXTENSION);
             }
         }
-        
-        
-        
+
+
+
         return stream;
     }
-    
-    public boolean evalScript(String scriptName) {
 
+    public boolean evalScript(String scriptName) throws InterventionException{
 
         boolean scriptOk = false;
+        
         try {
+            
             CompiledScript script = getCompiledScript(scriptName);
+            
             if (script != null) {
-                
+
                 script.eval();
-                
+
                 scriptOk = true;
 
             }
         } catch (ScriptException ex) {
-            scriptOk = false;
-            LocalizedLogger.severe(DynamicScriptEngine.class.getName(), "script_exception_occurred", ex.getMessage(), scriptName);
+            
+           throw new InterventionException("script_exception_occurred",ex,scriptName,SCRIPT_EXTENSION,ex.getLineNumber());
         }
 
         return scriptOk;
